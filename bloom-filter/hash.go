04af71c8b2dc6898c6c92fee856ad2b3fb870610
Momir Milutinović <milutinm462@gmail.com>
2023-01-20 16:19:05 +0100

    [doc] Dokumentovao hash.go

--- bloom-filter/hash.go
+++ bloom-filter/hash.go
@@ -6,16 +6,19 @@ import (
 	"time"
 )
 
+// HashWithSeed predstavlja jednu hes funkciju
 type HashWithSeed struct {
 	Seed []byte
 }
 
+// Funkcija hesira podatke
 func (h HashWithSeed) Hash(data []byte) uint64 {
 	fn := md5.New()
 	fn.Write(append(data, h.Seed...))
 	return binary.BigEndian.Uint64(fn.Sum(nil))
 }
 
+// Funkcija kreira k razlicitih hes funkcija sa razlicitim seed-ovima
 func CreateHashFunctions(k uint) []HashWithSeed {
 	h := make([]HashWithSeed, k)
 	ts := uint(time.Now().Unix())
