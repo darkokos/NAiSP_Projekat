1f83bdef5b3952ac6e0a28f8ab2839eb014124ad
Momir Milutinović <milutinm462@gmail.com>
2023-01-20 15:37:57 +0100

    [add] Dodat test za bloom filter u memoriji

--- bloom-filter/bloom_filter_test.go
+++ bloom-filter/bloom_filter_test.go
@@ -1,42 +1,53 @@
 package bloomfilter
 
 import (
-	"bytes"
-	"encoding/gob"
-	"fmt"
 	"testing"
 )
 
+func TestWords(t *testing.T) {
+	// Test primer preuzet sa https://www.geeksforgeeks.org/bloom-filters-introduction-and-python-implementation/
 
-func TestBloomFilter(t *testing.T) {
-	fns := CreateHashFunctions(5)
+	// Reci koje se dodaju
+	word_present := []string{"abound", "abounds", "abundance", "abundant", "accessible",
+		"bloom", "blossom", "bolster", "bonny", "bonus", "bonuses",
+		"coherent", "cohesive", "colorful", "comely", "comfort",
+		"gems", "generosity", "generous", "generously", "genial"}
 
-	buf := &bytes.Buffer{}
-	encoder := gob.NewEncoder(buf)
-	decoder := gob.NewDecoder(buf)
+	// Reci koje se ne dodaju
+	word_absent := []string{"bluff", "cheater", "hate", "war", "humanity",
+		"racism", "hurt", "nuke", "gloomy", "facebook",
+		"geeksforgeeks", "twitter"}
 
-	for _, fn := range fns {
-		data := []byte("hello")
-		fmt.Println(fn.Hash(data))
-		err := encoder.Encode(fn)
-		if err != nil {
-			panic(err)
-		}
-		dfn := &HashWithSeed{}
-		err = decoder.Decode(dfn)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(dfn.Hash(data))
+	falsePositiveRate := 0.05
+	bitArrayLen := CalculateM(len(word_present), falsePositiveRate)
+	hashFnCount := CalculateK(len(word_present), bitArrayLen)
+
+	t.Log("Duzina niza bitova: ", bitArrayLen)
+	t.Log("Broj hes funkcija: ", hashFnCount)
+	t.Log("False-positive rate: ", falsePositiveRate)
+
+	bloomFilter := CreateBloomFilter(bitArrayLen, hashFnCount)
+
+	for _, word := range word_present {
+		bloomFilter.add([]byte(word))
 	}
 
-	fmt.Println("Penguin Bloom")
-	bf := CreateBloomFilter(1024, 3)
+	for _, word := range word_present {
+		found := bloomFilter.find([]byte(word))
+		if !found {
+			t.Fatalf(word, " je trebao da bude nadjen, a nije")
+		} else {
+			t.Log(word, " nadjen")
+		}
+	}
 
-	bf.add([]byte("bloomFilter"))
-	fmt.Println(bf.find([]byte("bloomFilter")))
-	fmt.Println(bf.find([]byte("boomFilter")))
-
-	fmt.Println()
+	for _, word := range word_absent {
+		found := bloomFilter.find([]byte(word))
+		if !found {
+			t.Log(word, " nije nadjen")
+		} else {
+			t.Log(word, " je false-positive")
+		}
+	}
 
 }
