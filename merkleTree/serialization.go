974abe7af32517867420b8f6c11570e084d6fe2d
CodeX-cmd <kuvizic.marko@gmail.com>
2023-01-20 20:52:55 +0100

    Implementacija Merkle Stabla
    Treba jos implementirati deserijalizaciju i doterati serijalizaciju

--- merkleTree/serialization.go
+++ merkleTree/serialization.go
@@ -1,9 +1,17 @@
 package merkleTree
 
+<<<<<<< HEAD
 var empty []byte
 var emptyVal = byte(42)
 
 func SerializeTree(root MerkleRoot) []byte {
+=======
+
+var empty []byte
+var emptyVal = byte(42)
+
+func serializeTree(root MerkleRoot) []byte{
+>>>>>>> 0682d82 (Implementacija Merkle Stabla)
 	var serialized []byte
 	for i := 0; i < 20; i++ {
 		empty = append(empty, emptyVal)
@@ -11,18 +19,34 @@ func SerializeTree(root MerkleRoot) []byte {
 	serializeNode(*root.root, &serialized)
 	return serialized
 }
+<<<<<<< HEAD
 func serializeNode(node Node, serialized *[]byte) {
+=======
+func serializeNode(node Node, serialized *[]byte){
+>>>>>>> 0682d82 (Implementacija Merkle Stabla)
 	*serialized = append(*serialized, node.data...)
 
 	if node.left != nil {
 		serializeNode(*node.left, serialized)
+<<<<<<< HEAD
 	} else {
+=======
+	}else{
+>>>>>>> 0682d82 (Implementacija Merkle Stabla)
 		*serialized = append(*serialized, empty...)
 	}
 
 	if node.right != nil {
 		serializeNode(*node.right, serialized)
+<<<<<<< HEAD
 	} else {
 		*serialized = append(*serialized, empty...)
 	}
 }
+=======
+	}else{
+		*serialized = append(*serialized, empty...)
+	}
+}
+
+>>>>>>> 0682d82 (Implementacija Merkle Stabla)
