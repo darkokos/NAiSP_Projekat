0db51ace9394086ff40a638606ffce3eda12fd22
CodeX-cmd <kuvizic.marko@gmail.com>
2023-01-20 20:52:55 +0100

    Implementacija Merkle Stabla
    Treba jos implementirati deserijalizaciju i doterati serijalizaciju

--- merkleTree/merkleTree_test.go
+++ merkleTree/merkleTree_test.go
@@ -1,14 +1,11 @@
 package merkleTree
+
 import "testing"
 import "fmt"
-func Test(t *testing.T){
-	root := CreateMerkleTree([][]byte{[]byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA")})
-	fmt.Print((*root.root).data, "\n")
-	serialized := SerializeTree(root)
-	fmt.Print(serialized)
-	fmt.Print(len(serialized), "\n")
-	deserialized := DeserializeTree(serialized)
-	fmt.Print((*deserialized.root).data, "\n")
-	fmt.Print(SerializeTree(deserialized), "\n")
 
+func Test(t *testing.T) {
+	root := CreateMerkleTree([][]byte{[]byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA")})
+	serialized := serializeTree(root)
+	fmt.Print(serialized)
+	fmt.Print(len(serialized))
 }
