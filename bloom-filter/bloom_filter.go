f4c26dd5a9b55508d93c91969b2f745ef37980bc
Momir Milutinović <milutinm462@gmail.com>
2023-01-20 16:14:24 +0100

    [doc] Reorganizovao dokumentacione komentare.

--- bloom-filter/bloom_filter.go
+++ bloom-filter/bloom_filter.go
@@ -10,12 +10,9 @@ type BloomFilter struct {
 // Maske za izolovanje pojedinacnih bitova
 var masks [8]byte = [8]byte{0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000}
 
+// Funkcija pravi bloom filter sa poljem bitova duzine bitArrayLen i
+// hashFunnctionCount hes funkcija.
 func CreateBloomFilter(bitArrayLen uint, hashFunctionCount uint) BloomFilter {
-	/*
-		Funkcija pravi bloom filter sa poljem bitova duzine bitArrayLen i
-		hashFunnctionCount hes funkcija.
-	*/
-
 	BitField := make([]byte, bitArrayLen/8+bitArrayLen%8) // Ako broj bitova nije deljiv sa 8, zaokruzujemo broj bajtova na gore da bi se alociralo dovoljno prostora.
 
 	hashFunctions := CreateHashFunctions(hashFunctionCount)
@@ -35,11 +32,9 @@ func CreateBloomFilterBasedOnParams(expectedElements int, falsePositiveRate floa
 	return CreateBloomFilter(bitArrayLen, hashFnCount)
 }
 
+// Funkcija dekomponuje indeks bita u polju bitova na indeks bajta i
+// indeks bita u tom bajtu.
 func bitAndByteIndex(bitFieldIndex uint64, bitFieldLen uint64) (byteIndex uint64, bitIndex uint64) {
-	/*
-		Funkcija dekomponuje indeks bita u polju bitova na indeks bajta i
-		indeks bita u tom bajtu.
-	*/
 
 	bitIndex = bitFieldIndex % bitFieldLen
 	//fmt.Println("Bit checking")
@@ -55,10 +50,8 @@ func bitAndByteIndex(bitFieldIndex uint64, bitFieldLen uint64) (byteIndex uint64
 	return byteIndex, bitIndex
 }
 
+//Funkcija dodaje element u bloom filter.
 func (bloomFilter BloomFilter) add(key []byte) {
-	/*
-		Funkcija dodaje element u bloom filter.
-	*/
 
 	//bitIndices := make(uint64[], bloomFilter.HashFunctionCount)
 	for _, hashFn := range bloomFilter.HashFunctions {
@@ -69,11 +62,9 @@ func (bloomFilter BloomFilter) add(key []byte) {
 	}
 }
 
+// Funkcija proverava da li je element mozda prisutan u bloom filteru.
+// Vraca true ako element mozda jeste prisutan, a false ako sigurno nije.
 func (bloomFilter BloomFilter) find(key []byte) bool {
-	/*
-		Funkcija proverava da li je element mozda prisutan u bloom filteru.
-		Vraca true ako element mozda jeste prisutan, a false ako sigurno nije.
-	*/
 
 	for _, hashFn := range bloomFilter.HashFunctions {
 		byteIndex, bitIndex := bitAndByteIndex(hashFn.Hash(key), uint64(bloomFilter.BitArrayLen))
