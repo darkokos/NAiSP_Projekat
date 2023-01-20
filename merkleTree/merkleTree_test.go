fed5abd236e0c0608368c0afe19db07b31919f5b
CodeX-cmd <kuvizic.marko@gmail.com>
2023-01-20 20:52:55 +0100

    Implementacija Merkle Stabla
    Treba jos implementirati deserijalizaciju i doterati serijalizaciju

--- merkleTree/merkleTree_test.go
+++ merkleTree/merkleTree_test.go
@@ -3,6 +3,7 @@ import "testing"
 import "fmt"
 func Test(t *testing.T){
 	root := CreateMerkleTree([][]byte{[]byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA")})
+<<<<<<< HEAD
 	fmt.Print((*root.root).data, "\n")
 	serialized := SerializeTree(root)
 	fmt.Print(serialized)
@@ -11,4 +12,9 @@ func Test(t *testing.T){
 	fmt.Print((*deserialized.root).data, "\n")
 	fmt.Print(SerializeTree(deserialized), "\n")
 
+=======
+	serialized := serializeTree(root)
+	fmt.Print(serialized)
+	fmt.Print(len(serialized))
+>>>>>>> 004f0d9 (Implementacija Merkle Stabla)
 }
--- merkleTree/serialization.go
+++ merkleTree/serialization.go
@@ -1,6 +1,7 @@
 package merkleTree
 
 
+<<<<<<< HEAD
 var hashSize = 20	//Velicina jednog zapisa, u vidu broja bajtova, u specifikaciji je 20
 //Funkcija radi serijalizaciju stabla iz korena
 func SerializeTree(root MerkleRoot) []byte{
@@ -56,3 +57,32 @@ func DeserializeLevel(level []byte, previousLevel []*Node) []*Node{
 	}
 	return currentLevel
 }
+=======
+var empty []byte
+var emptyVal = byte(42)
+
+func serializeTree(root MerkleRoot) []byte{
+	var serialized []byte
+	for i := 0; i < 20; i++ {
+		empty = append(empty, emptyVal)
+	}
+	serializeNode(*root.root, &serialized)
+	return serialized
+}
+func serializeNode(node Node, serialized *[]byte){
+	*serialized = append(*serialized, node.data...)
+
+	if node.left != nil {
+		serializeNode(*node.left, serialized)
+	}else{
+		*serialized = append(*serialized, empty...)
+	}
+
+	if node.right != nil {
+		serializeNode(*node.right, serialized)
+	}else{
+		*serialized = append(*serialized, empty...)
+	}
+}
+
+>>>>>>> 004f0d9 (Implementacija Merkle Stabla)
