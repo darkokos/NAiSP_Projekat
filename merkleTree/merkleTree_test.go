362b40b74bd728e403cae0b50da21d399493546b
CodeX-cmd <kuvizic.marko@gmail.com>
2023-01-21 21:00:31 +0100

    [add] Deserijalizacija uradjena, serijalizacija poboljsana
    Ima mesta za optimizaciju

--- merkleTree/merkleTree_test.go
+++ merkleTree/merkleTree_test.go
@@ -5,7 +5,10 @@ import "fmt"
 
 func Test(t *testing.T) {
 	root := CreateMerkleTree([][]byte{[]byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA")})
-	serialized := serializeTree(root)
+	serialized := SerializeTree(root)
 	fmt.Print(serialized)
-	fmt.Print(len(serialized))
+	fmt.Print(len(serialized), "\n")
+	deserialized := DeserializeTree(serialized)
+	fmt.Print(SerializeTree(deserialized), "\n")
+
 }
