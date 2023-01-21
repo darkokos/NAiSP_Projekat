7f3611ba52e3e50d100a86dbf9dd3aa25e33bdd3
CodeX-cmd <kuvizic.marko@gmail.com>
2023-01-21 21:12:03 +0100

    [doc] Dodata dokumentacija za serijalizaciju i deserijalizaciju merkle stabla

--- merkleTree/merkleTree_test.go
+++ merkleTree/merkleTree_test.go
@@ -5,10 +5,12 @@ import "fmt"
 
 func Test(t *testing.T) {
 	root := CreateMerkleTree([][]byte{[]byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA")})
+	fmt.Print((*root.root).data, "\n")
 	serialized := SerializeTree(root)
 	fmt.Print(serialized)
 	fmt.Print(len(serialized), "\n")
 	deserialized := DeserializeTree(serialized)
+	fmt.Print((*deserialized.root).data, "\n")
 	fmt.Print(SerializeTree(deserialized), "\n")
 
 }
--- merkleTree/serialization.go
+++ merkleTree/serialization.go
@@ -1,36 +1,9 @@
 package merkleTree
 
-var empty []byte
-var emptyVal = byte(42)
-var emptyNode = Node{}
-
-// func serializeTree(root MerkleRoot) []byte{
-// 	var serialized []byte
-// 	for i := 0; i < 20; i++ {
-// 		empty = append(empty, emptyVal)
-// 	}
-// 	serializeNode(*root.root, &serialized)
-// 	return serialized
-// }
-// func serializeNode(node Node, serialized *[]byte){
-// 	*serialized = append(*serialized, node.data...)
-
-// 	if node.left != nil {
-// 		serializeNode(*node.left, serialized)
-// 	}else{
-// 		*serialized = append(*serialized, empty...)
-// 	}
-
-//		if node.right != nil {
-//			serializeNode(*node.right, serialized)
-//		}else{
-//			*serialized = append(*serialized, empty...)
-//		}
-//	}
+var hashSize = 20 //Velicina jednog zapisa, u vidu broja bajtova, u specifikaciji je 20
+// Funkcija radi serijalizaciju stabla iz korena
 func SerializeTree(root MerkleRoot) []byte {
-	for i := 0; i < 20; i++ {
-		empty = append(empty, emptyVal)
-	}
+
 	return serialize([]Node{*root.root}, &[]byte{})
 }
 func serialize(level []Node, serialized *[]byte) []byte {
@@ -40,45 +13,46 @@ func serialize(level []Node, serialized *[]byte) []byte {
 	}
 	var nextLevel []Node
 	for _, node := range level {
-		if node.data == nil {
-			*serialized = append(*serialized, empty...)
-		} else {
+		if node.data != nil { //Common sense provera, verovatno nije potrebna
 			*serialized = append(*serialized, node.data...)
 		}
 		if node.left != nil {
 			nextLevel = append(nextLevel, *node.left)
 		}
-
 		if node.right != nil {
 			nextLevel = append(nextLevel, *node.left)
 		}
 	}
 	return serialize(nextLevel, serialized)
 }
+
+// Deserijalizacija stabla na osnovu niza bajtova
 func DeserializeTree(serializedTree []byte) MerkleRoot {
 
-	rootNode := Node{data: serializedTree[0:20]}
+	rootNode := Node{data: serializedTree[0:hashSize]} //Konstrukcija korena
 	root := MerkleRoot{root: &rootNode}
-	level := []*Node{&rootNode}
-	serializedTree = serializedTree[20:]
+	level := []*Node{&rootNode} //Posto je serijalizacija uradjena bfs prolazom, niz bajtova delimo na nivoe stabla
+	serializedTree = serializedTree[hashSize:]
 
 	for i := 1; len(serializedTree) != 0; i *= 2 {
-		level = DeserializeLevel(serializedTree[20:(i*2+1)*20], level)
-		serializedTree = serializedTree[(i*2)*20:]
+		level = DeserializeLevel(serializedTree[hashSize:(i*2+1)*hashSize], level) //izdvajanje jednog nivoa
+		serializedTree = serializedTree[(i*2)*hashSize:]                           //popovanje tog nivoa iz niza
 	}
 	return root
 }
+
+// Funkcija deserijalizuje jedan nivo stabla
 func DeserializeLevel(level []byte, previousLevel []*Node) []*Node {
 
 	var node Node
 	var currentLevel []*Node
-	for i := 0; i < len(level)/40; i += 40 {
-		node.data = level[i : i+20]
-		previousLevel[i/20].left = &node
+	for i := 0; i < len(level)/(2*hashSize); i += (2 * hashSize) { //Svaka dva zapisa u nivou su redom levo i desno dete odgovarajuceg cvora u nivou iznad
+		node.data = level[i : i+hashSize]
+		previousLevel[i/hashSize].left = &node
 		currentLevel = append(currentLevel, &node)
 
-		node.data = level[i+20 : i+40]
-		previousLevel[i/20].right = &node
+		node.data = level[i+hashSize : i+(2*hashSize)]
+		previousLevel[i/hashSize].right = &node
 		currentLevel = append(currentLevel, &node)
 	}
 	return currentLevel
