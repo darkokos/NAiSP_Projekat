ee54c1283846d5e120704ac8ade3f5a908485182
CodeX-cmd <kuvizic.marko@gmail.com>
2023-01-22 20:51:21 +0100

    [Add]Implementirana specijalna dvostruko povezana lista za kes, i sam kes
    Kes nije zavrsen, treba da pristupa sstabeli da bi se punio, tako da nije ni istestiran do kraja

--- LRU_cache/dll.go
+++ LRU_cache/dll.go
@@ -3,73 +3,71 @@ package LRU_cache
 //Ovaj faj implementira dvostruko povezanu listu, za lru cache
 
 type Dll struct {
-	head *DllElement
-	tail *DllElement
+	head     *DllElement
+	tail     *DllElement
 	capacity int
-	size int
+	size     int
 }
-//Jedan element 
+
+// Jedan element
 type DllElement struct {
 	data []byte
 	next *DllElement
 	prev *DllElement
 }
 
-
-func (list *Dll) Init(size int){
+func (list *Dll) Init(size int) {
 	(*list).capacity = size
 	(*list).size = 0
 }
-func (list *Dll) IsFull() bool{
+func (list *Dll) IsFull() bool {
 	return (*list).size == (*list).capacity
 }
-func (list *Dll) Pop(n int) DllElement{
+func (list *Dll) Pop(n int) DllElement {
 	(*list).size--
 	var cursor DllElement
 	var x int
-	if n == 0{
+	if n == 0 {
 		cursor = *(*list).head
 		(*list).head = (*list.head).next
 		return cursor
-	}else if n == list.capacity {
+	} else if n == list.capacity {
 		cursor = *(*list).tail
 		(*list).tail = (*list.tail).prev
 		return cursor
 	}
-	if (n < list.capacity/2){
+	if n < list.capacity/2 {
 		cursor = *(*list).head
 		x = 1
-	}else{
+	} else {
 		cursor = *(*list).tail
 		x = -1
 	}
-	for i := 0; i != n; i+=x{
+	for i := 0; i != n; i += x {
 		cursor = *cursor.next
 	}
 	(*cursor.prev).next = cursor.next
 	(*cursor.next).prev = cursor.prev
-	
-	cursor.next = nil	//Common sense da bi se izbegle duplirane reference, verovatno nije neophodno
+
+	cursor.next = nil //Common sense da bi se izbegle duplirane reference, verovatno nije neophodno
 	cursor.prev = nil
 	return cursor
 }
 
-//Jedan push prima samo podatke pa konstruise element, a drugi prima vec konstruisan element (go ne podrzava preklapanje metoda, grrrr)
-func (list *Dll) Push(data []byte){
+// Jedan push prima samo podatke pa konstruise element, a drugi prima vec konstruisan element (go ne podrzava preklapanje metoda, grrrr)
+func (list *Dll) Push(data []byte) {
 	(*list).size++
-	el := DllElement{data:data}
-	if(list.size == 1){
-		list.head = &el 
-		list.tail = &el 
+	el := DllElement{data: data}
+	if list.size == 1 {
+		list.head = &el
 		return
 	}
 	(*list.head).prev = &el
 	el.next = list.head
 	(*list).head = &el
 }
-
-func (list *Dll) PushNode(el DllElement){
-	if((*list).IsFull()){
+func (list *Dll) PushNode(el DllElement) {
+	if (*list).IsFull() {
 		(*list).DeleteLast()
 	}
 	(*list).size++
@@ -89,7 +87,4 @@ func (list *Dll) DeleteLast() {
 	last.prev = nil
 	last.next = nil
 	(*list).size--
-}
-func (list *Dll) GetLast() []byte {
-	return (*list.tail).data
 }
