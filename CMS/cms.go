0bb47c5d7baf1d0c1036096bf671aecae7458dba
CodeX-cmd <kuvizic.marko@gmail.com>
2023-01-25 14:17:28 +0100

    [Add] Resena deserijalizacija CMS-a

--- CMS/cms.go
+++ CMS/cms.go
@@ -20,6 +20,7 @@ func (cms *CMS) Init(precision float64, certainty float64){
 		(*cms).table[i] = make([]uint, (*cms).m)
 	}
 	(*cms).hashes = CreateHashFunctions((*cms).k)
+	
 }
 func (cms *CMS) Add(key []byte){
 	var j uint64
@@ -53,18 +54,34 @@ func (cms *CMS) Serialize() []byte{
 
 	return ret
 } 
+func Deserialize(buf [] byte) CMS{
+	precision := deserializeFloat(buf[0:8])
+	certainty := deserializeFloat(buf[8:16])
+	cms := CMS{}
+	cms.Init(precision, certainty)
+	x := 16
+	for i := range cms.table {
+		for j := range cms.table[i] {
+			
+			cms.table[i][j] = uint(deserializeUint(buf[x:x+4]))
+			x += 4
+		}
+	}
+	return cms
+}
 func serializeUint(x uint32) []byte{
 	a := make([]byte, 4)
-	binary.BigEndian.PutUint32(a, x)
+	binary.LittleEndian.PutUint32(a, x)
 	return a
 }
 func serializeFloat(f float64) []byte{
-	var buf [8]byte
-	kn := math.Float64bits(f)
-
-	for i := 0; i < 8; i++ {
-		buf[i] = byte(kn >> (7-i)*8)
-	}
-
-	return buf[:]
+    ret := make([]byte, 8)
+    binary.LittleEndian.PutUint64(ret, math.Float64bits(f))
+    return ret
+}
+func deserializeFloat(buf []byte) float64{
+    return math.Float64frombits(binary.LittleEndian.Uint64(buf))
+}
+func deserializeUint(buf []byte) uint32{
+	return binary.LittleEndian.Uint32(buf)
 }
