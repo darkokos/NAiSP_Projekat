af404402b70d388f293047e10990f5723914ab7a
CodeX-cmd <kuvizic.marko@gmail.com>
2023-01-25 14:17:28 +0100

    [Add] Resena deserijalizacija CMS-a

--- CMS/cms_test.go
+++ CMS/cms_test.go
@@ -7,5 +7,9 @@ func Test(t *testing.T){
 	cms.Add([]byte{1, 2})
 	cms.Add([]byte{1, 2})
 	cms.Add([]byte{1, 2})
+	fmt.Print(cms.Serialize())
 	fmt.Print(cms.Read([]byte{1, 2}))
+	cms2 := Deserialize(cms.Serialize())
+	fmt.Print("\n", cms2.Serialize())
+	fmt.Print(cms2.Read([]byte{1, 2}))
 }
