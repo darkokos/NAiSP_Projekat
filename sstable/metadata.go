9944e5b0700769d9cf05178d52fa1ba10e326885
Momir Milutinović <milutinm462@gmail.com>
2023-01-28 21:14:35 +0100

    [add] Zapisivanje merkle stabla u zaseban fajl

---
+++ sstable/metadata.go
@@ -0,0 +1,37 @@
+package sstable
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+
+	"github.com/darkokos/NAiSP_Projekat/memtable"
+	"github.com/darkokos/NAiSP_Projekat/merkleTree"
+)
+
+func writeMetadataSeparateFile(entries []*memtable.MemTableEntry, filename string) {
+	data := make([][]byte, len(entries))
+
+	for i, entry := range entries {
+		data[i] = make([]byte, len(entry.Value))
+		copy(data[i], entry.Value)
+	}
+
+	metadata := merkleTree.CreateMerkleTree(data)
+
+	metadata_bytes := merkleTree.SerializeTree(metadata)
+
+	metdata_file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("Greska u otvaranju fajla za merkle stablo")
+		return // TODO: Sta se ako dodje do greske u kreiranju merkle stabla
+	}
+
+	err = binary.Write(metdata_file, binary.LittleEndian, metadata_bytes)
+	if err != nil {
+		fmt.Println("Greska u zapsivanju merkle stabla")
+		return
+	}
+
+	metdata_file.Close()
+}
