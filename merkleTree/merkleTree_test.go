5c658b5b951e5f508b235c2545630cc9e1c36379
Momir Milutinović <milutinm462@gmail.com>
2023-01-30 19:46:23 +0100

    [fix] Export SerializeTree u merkleTree

--- merkleTree/merkleTree_test.go
+++ merkleTree/merkleTree_test.go
@@ -1,20 +1,13 @@
 package merkleTree
-import "testing"
-import "fmt"
-func Test(t *testing.T){
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test(t *testing.T) {
 	root := CreateMerkleTree([][]byte{[]byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA")})
-<<<<<<< HEAD
-	fmt.Print((*root.root).data, "\n")
 	serialized := SerializeTree(root)
 	fmt.Print(serialized)
-	fmt.Print(len(serialized), "\n")
-	deserialized := DeserializeTree(serialized)
-	fmt.Print((*deserialized.root).data, "\n")
-	fmt.Print(SerializeTree(deserialized), "\n")
-
-=======
-	serialized := serializeTree(root)
-	fmt.Print(serialized)
 	fmt.Print(len(serialized))
->>>>>>> 004f0d9 (Implementacija Merkle Stabla)
 }
