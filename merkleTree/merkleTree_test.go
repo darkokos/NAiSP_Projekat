9019f56660a7f87ee59abdbdd26ffe0140cd2923
Momir Milutinović <milutinm462@gmail.com>
2023-01-30 19:46:23 +0100

    [fix] Export SerializeTree u merkleTree

--- merkleTree/merkleTree_test.go
+++ merkleTree/merkleTree_test.go
@@ -7,12 +7,7 @@ import (
 
 func Test(t *testing.T) {
 	root := CreateMerkleTree([][]byte{[]byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA")})
-	fmt.Print((*root.root).data, "\n")
 	serialized := SerializeTree(root)
 	fmt.Print(serialized)
-	fmt.Print(len(serialized), "\n")
-	deserialized := DeserializeTree(serialized)
-	fmt.Print((*deserialized.root).data, "\n")
-	fmt.Print(SerializeTree(deserialized), "\n")
-
+	fmt.Print(len(serialized))
 }
