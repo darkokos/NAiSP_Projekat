9a8c562569669dd8c97cc4150502b0332363de95
Momir Milutinović <milutinm462@gmail.com>
2023-01-30 19:46:23 +0100

    [fix] Export SerializeTree u merkleTree

--- merkleTree/merkleTree_test.go
+++ merkleTree/merkleTree_test.go
@@ -1,22 +1,13 @@
 package merkleTree
 
-import "testing"
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test(t *testing.T) {
 	root := CreateMerkleTree([][]byte{[]byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA"), []byte("BLA")})
-<<<<<<< HEAD
-	fmt.Print((*root.root).data, "\n")
 	serialized := SerializeTree(root)
 	fmt.Print(serialized)
-	fmt.Print(len(serialized), "\n")
-	deserialized := DeserializeTree(serialized)
-	fmt.Print((*deserialized.root).data, "\n")
-	fmt.Print(SerializeTree(deserialized), "\n")
-
-=======
-	serialized := serializeTree(root)
-	fmt.Print(serialized)
 	fmt.Print(len(serialized))
->>>>>>> 004f0d9 (Implementacija Merkle Stabla)
 }
--- merkleTree/serialization.go
+++ merkleTree/serialization.go
@@ -1,71 +1,9 @@
 package merkleTree
 
-var hashSize = 20 //Velicina jednog zapisa, u vidu broja bajtova, u specifikaciji je 20
-// Funkcija radi serijalizaciju stabla iz korena
-func SerializeTree(root MerkleRoot) []byte {
-
-
-var hashSize = 20	//Velicina jednog zapisa, u vidu broja bajtova, u specifikaciji je 20
-//Funkcija radi serijalizaciju stabla iz korena
-func SerializeTree(root MerkleRoot) []byte{
-	return serialize([]Node{*root.root}, &[]byte{})
-}
-func serialize(level []Node, serialized *[]byte) []byte {
-
-	if len(level) == 0 {
-		return *serialized
-	}
-	var nextLevel []Node
-	for _, node := range level {
-		if node.data != nil { //Common sense provera, verovatno nije potrebna
-			*serialized = append(*serialized, node.data...)
-		}
-		if node.left != nil {
-			nextLevel = append(nextLevel, *node.left)
-		}
-		if node.right != nil {
-			nextLevel = append(nextLevel, *node.left)
-		}
-	}
-	return serialize(nextLevel, serialized)
-}
-
-// Deserijalizacija stabla na osnovu niza bajtova
-func DeserializeTree(serializedTree []byte) MerkleRoot {
-
-	rootNode := Node{data: serializedTree[0:hashSize]} //Konstrukcija korena
-	root := MerkleRoot{root: &rootNode}
-	level := []*Node{&rootNode} //Posto je serijalizacija uradjena bfs prolazom, niz bajtova delimo na nivoe stabla
-	serializedTree = serializedTree[hashSize:]
-
-	for i := 1; len(serializedTree) != 0; i *= 2 {
-		level = DeserializeLevel(serializedTree[hashSize:(i*2+1)*hashSize], level) //izdvajanje jednog nivoa
-		serializedTree = serializedTree[(i*2)*hashSize:]                           //popovanje tog nivoa iz niza
-	}
-	return root
-}
-
-// Funkcija deserijalizuje jedan nivo stabla
-func DeserializeLevel(level []byte, previousLevel []*Node) []*Node {
-
-	var node Node
-	var currentLevel []*Node
-	for i := 0; i < len(level)/(2*hashSize); i += (2 * hashSize) { //Svaka dva zapisa u nivou su redom levo i desno dete odgovarajuceg cvora u nivou iznad
-		node.data = level[i : i+hashSize]
-		previousLevel[i/hashSize].left = &node
-		currentLevel = append(currentLevel, &node)
-
-		node.data = level[i+hashSize : i+(2*hashSize)]
-		previousLevel[i/hashSize].right = &node
-		currentLevel = append(currentLevel, &node)
-	}
-	return currentLevel
-}
-
 var empty []byte
 var emptyVal = byte(42)
 
-func serializeTree(root MerkleRoot) []byte{
+func SerializeTree(root MerkleRoot) []byte {
 	var serialized []byte
 	for i := 0; i < 20; i++ {
 		empty = append(empty, emptyVal)
@@ -73,18 +11,18 @@ func serializeTree(root MerkleRoot) []byte{
 	serializeNode(*root.root, &serialized)
 	return serialized
 }
-func serializeNode(node Node, serialized *[]byte){
+func serializeNode(node Node, serialized *[]byte) {
 	*serialized = append(*serialized, node.data...)
 
 	if node.left != nil {
 		serializeNode(*node.left, serialized)
-	}else{
+	} else {
 		*serialized = append(*serialized, empty...)
 	}
 
 	if node.right != nil {
 		serializeNode(*node.right, serialized)
-	}else{
+	} else {
 		*serialized = append(*serialized, empty...)
 	}
 }
