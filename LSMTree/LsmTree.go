301033ae00cc8aa61953506ae501dddd6901a131
Gojko <vuckovic.gojko1@gmail.com>
2023-01-30 22:49:41 +0100

    [add] Dodate nove stvari za LSMTree

--- LSMTree/LsmTree.go
+++ LSMTree/LsmTree.go
@@ -1,67 +1,86 @@
-//TODO: Implementirati da radi sa nasim memtable i sstables
+//TODO: -Implementirati da uzima maksimalan nivo iz configa
+//      -Pravljenje SSTabela(mada ne znam da li ovo treba u memtable da se implementira ili ovde)
 
 package lsmtree
 
 import (
-	"bytes"
-	"encoding/binary"
+	"fmt"
+	"strconv"
+
+	memtable "github.com/darkokos/NAiSP_Projekat/memtable"
+	sstable "github.com/darkokos/NAiSP_Projekat/sstable"
 )
 
-// Ovo je samo prototip kako bi trebalo da radi
+// Pravi LSMTree sa memtabelom,ss tabelama,maksimalnim nivoom i trenutnim najvecim nivoom
 type LogStructuredMergeTree struct {
-	memtable       map[string][]byte
-	sstables       [][]byte
-	mergeThreshold int
+	memtable     memtable.MemTable
+	sstables     []string
+	level        int
+	currentlevel int
 }
 
-func NewLogStructuredMergeTree(mergeThreshold int) *LogStructuredMergeTree {
+func NewLogStructuredMergeTree(capacity int, LSMlevel int) *LogStructuredMergeTree {
 	return &LogStructuredMergeTree{
-		memtable:       make(map[string][]byte),
-		sstables:       [][]byte{},
-		mergeThreshold: mergeThreshold,
+		memtable:     *memtable.MakeHashMapMemTable(capacity),
+		sstables:     make([]string, 0),
+		level:        LSMlevel,
+		currentlevel: findlevel(),
 	}
 }
 
-func (lsmt *LogStructuredMergeTree) Put(key []byte, value []byte) { //stavlja entry u memtable, a ako je pun pravi novu sstabelu
-	lsmt.memtable[string(key)] = value
-	if len(lsmt.memtable) >= lsmt.mergeThreshold {
-
-		lsmt.merge()
+// Trazi trenutni najveci nivo
+func findlevel() int {
+	i := 1
+	for {
+		iter := sstable.GetSSTableIterator("level-" + fmt.Sprint(i) + "-usertable-000001-Data.db")
+		if iter == nil {
+			return i
+		}
+		i++
 	}
 }
 
+// Trazi u memtable, ako ne nadje trazi u ss tabelama
 func (lsmt *LogStructuredMergeTree) Get(key []byte) (string, bool) { //trazi prvo u memtable, ako nije tamo prolazi kroz svaki sstable
 
-	if value, ok := lsmt.memtable[string(key)]; ok {
+	if value, ok := lsmt.memtable.Get(string(key)); ok {
 		return string(value), true
+	} else {
+		return lsmt.FindInSSTable(key)
+
 	}
-	for _, sstable := range lsmt.sstables {
-		if len(sstable) < len(key)+4 {
-			continue
-		}
-		if bytes.Compare(sstable[:len(key)], key) == 0 {
-			valueSize := binary.LittleEndian.Uint32(sstable[len(key) : len(key)+4])
-			valueStart := len(key) + 4
-			valueEnd := valueStart + int(valueSize)
-			if len(sstable) < valueEnd {
-				continue
-			}
-			return string(sstable[valueStart:valueEnd]), true
-		}
-	}
-	return "", false
+
 }
 
-func (lsmt *LogStructuredMergeTree) merge() { //pravljenje sstabele
-	var newSSTable []byte
-	for key, value := range lsmt.memtable {
-		k := []byte(key)
-		newSSTable = append(newSSTable, k...)
-		var valueSize = make([]byte, 4)
-		binary.LittleEndian.PutUint32(valueSize, uint32(len(value)))
-		newSSTable = append(newSSTable, valueSize...)
-		newSSTable = append(newSSTable, value...)
+// Trazi kljuc u svim SS tabelama
+func (lsmt *LogStructuredMergeTree) FindInSSTable(key []byte) (string, bool) {
+	i := 1
+	for i <= lsmt.currentlevel {
+		levelstr := ""
+		if i < 10 {
+			levelstr = "0" + fmt.Sprint(i)
+		} else {
+			levelstr = fmt.Sprint(i)
+		}
+		if i == 1 {
+			iter := sstable.GetSSTableIterator("level-" + levelstr + "-usertable-000001-Data.db")
+			for entry := iter.Next(); iter.Valid; entry = iter.Next() {
+				if string(entry.Key) == string(key) {
+					return string(entry.Value), true
+				}
+			}
+		} else {
+			for j := 1; j < 3; j++ {
+				iter := sstable.GetSSTableIterator("level-" + levelstr + "-usertable-00000" + strconv.Itoa(j) + "-Data.db")
+				for entry := iter.Next(); iter.Valid; entry = iter.Next() {
+					if string(entry.Key) == string(key) {
+						return string(entry.Value), true
+					}
+				}
+			}
+		}
+
 	}
-	lsmt.sstables = append(lsmt.sstables, newSSTable)
-	lsmt.memtable = make(map[string][]byte)
+	fmt.Println("Ne nalazi se ni u jednoj sstabeli niti memtable")
+	return "", false
 }
