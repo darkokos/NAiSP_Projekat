492fe344a81d86801545174b9e31a545c13e646e
Momir Milutinović <milutinm462@gmail.com>
2023-01-30 19:26:37 +0100

    [add] Citanje konfiguracionog fajla

---
+++ config/readConfig_test.go
@@ -0,0 +1,36 @@
+package config
+
+import "testing"
+
+func TestReadConfig(t *testing.T) {
+
+	ReadConfig()
+
+	if config.WalSize != 20 {
+		t.Fatalf("Pogresno je procitana velicina wal segmenta")
+	}
+
+	if config.MemtableSize != 10 {
+		t.Fatalf("Pogresno je procitana velicina memtable")
+	}
+
+	if config.MemtableStructure != "btree" {
+		t.Fatalf("Pogresno je procitana koja struktura se koristi za memtable")
+	}
+
+	if config.LSMTreeLevels != 4 {
+		t.Fatalf("Pogresno je procitan broj nivoa LSM stabla")
+	}
+
+	if config.SummaryDensity != 5 {
+		t.Fatalf("Nije dobro procitana popunjenost summary-a")
+	}
+
+	if config.CacheSize != 4 {
+		t.Fatalf("Nije dobro procitana popunjenost cache-a")
+	}
+
+	if config.RateLimit != 4 {
+		t.Fatalf("Nije dobro procitan rate limit")
+	}
+}
