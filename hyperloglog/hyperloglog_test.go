d9ea10abee7bc3131338c6eabbc2ea2f2351a8dd
Momir Milutinović <milutinm462@gmail.com>
2023-02-01 12:52:48 +0100

    [add] Serijalizacija i deserijalizacija HyperLogLog-a

--- hyperloglog/hyperloglog_test.go
+++ hyperloglog/hyperloglog_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestHyperLogLog(t *testing.T) {
-	hll := CreateHLL(12)
+	hll := CreateHLL(16)
 	rand.Seed(time.Now().Unix())
 	number_of_elements := 100000
 	for i := 0; i < number_of_elements; i++ {
@@ -43,4 +43,48 @@ func TestHyperLogLog(t *testing.T) {
 
 	fmt.Println("M")
 	fmt.Println(hll.m)
+}
+
+func TestHyperLogLogSerializeDeserialize(t *testing.T) {
+	hll := CreateHLL(12)
+	rand.Seed(time.Now().Unix())
+	number_of_elements := 100000
+	for i := 0; i < number_of_elements; i++ {
+
+		length := 10000
+
+		ran_str := make([]byte, length)
+
+		// Generating Random string
+		for i := 0; i < length; i++ {
+			ran_str[i] = byte(65 + rand.Intn(25))
+		}
+
+		hll.Add(ran_str)
+	}
+
+	estimate := hll.Estimate()
+
+	serialized_bytes := hll.Serialize()
+	if serialized_bytes == nil {
+		t.Fatalf("Serijalizacija nije uspela")
+	}
+
+	deserialized_hll := DeserializeHLL(serialized_bytes)
+
+	if deserialized_hll == nil {
+		t.Fatalf("Deserijalizacija HLL-a je neuspesna")
+	}
+
+	if deserialized_hll.Estimate() != estimate {
+		t.Fatalf("Brojevi elemenata serijalizovanog i deserijalizovanog hll-a se razlikuju")
+	}
+}
+
+func TestHyperLogLogDeserializeRadnomBytes(t *testing.T) {
+	bytes := []byte{0, 35, 5, 1, 6, 1, 6, 7, 1, 2}
+
+	if DeserializeHLL(bytes) != nil {
+		t.Fatalf("Ovo nije trebalo da se deserijalizuje")
+	}
 }
