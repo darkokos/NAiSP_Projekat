c7ef3d680fa22ae66cf97e3f52b9950fedf919c5
Momir Milutinović <milutinm462@gmail.com>
2023-02-05 19:36:49 +0100

    [add] Dodat bloom filter tip

---
+++ engine/bloom_filter_type.go
@@ -0,0 +1,38 @@
+package engine
+
+import bloomfilter "github.com/darkokos/NAiSP_Projekat/bloom-filter"
+
+func TransformKeyToBloomFilterKey(key string) string {
+	return "bloomfilter." + key
+}
+
+func (engine *DB) CreateBloomFilter(key string, m uint, k uint) {
+
+	bloom_filter := bloomfilter.CreateBloomFilter(m, k)
+	serialized_bloom_filter := bloom_filter.Serialize()
+
+	engine.Put(TransformKeyToBloomFilterKey(key), serialized_bloom_filter)
+}
+
+func (engine *DB) IsStringPossiblyInBloomFilter(key string, value string) (possibly_present, bloom_filter_exists bool) {
+	serialized_bloom_filter := engine.Get(TransformKeyToBloomFilterKey(key))
+
+	if serialized_bloom_filter != nil {
+		bloom_filter := bloomfilter.Deserialize(serialized_bloom_filter)
+		return bloom_filter.Find([]byte(value)), true
+	} else {
+		return false, false
+	}
+}
+
+func (engine *DB) AddStringToBloomFilter(key string, new_value string) bool {
+	serialized_bloom_filter := engine.Get(TransformKeyToBloomFilterKey(key))
+
+	if serialized_bloom_filter != nil {
+		bloom_filter := bloomfilter.Deserialize(serialized_bloom_filter)
+		bloom_filter.Add([]byte(new_value))
+		return engine.Put(TransformKeyToBloomFilterKey(key), bloom_filter.Serialize())
+	} else {
+		return false
+	}
+}
---
+++ engine/bloom_filter_type_test.go
@@ -0,0 +1,57 @@
+package engine
+
+import (
+	"testing"
+
+	"github.com/darkokos/NAiSP_Projekat/config"
+)
+
+func TestBloomFilterType(t *testing.T) {
+	config.DefaultConfiguration.MemtableSize = 2 // Da se bloom filter odmah flushuje na disk
+	config.DefaultConfiguration.RateLimit = 9999
+	config.DefaultConfiguration.MultipleFileSSTable = true
+	config.ReadConfig()
+	Cleanup()
+
+	db := GetNewDB()
+
+	bf_key := "mojFilter"
+	db.CreateBloomFilter(bf_key, 76, 4)
+	db.Put("1", []byte{}) // Flush bloom filter
+
+	// Reci koje se dodaju
+	word_present := []string{"abound", "abounds", "abundance", "abundant", "accessible",
+		"bloom", "blossom", "bolster", "bonny", "bonus", "bonuses",
+		"coherent", "cohesive", "colorful", "comely", "comfort",
+		"gems", "generosity", "generous", "generously", "genial"}
+
+	// Reci koje se ne dodaju
+	word_absent := []string{"bluff", "cheater", "hate", "war", "humanity",
+		"racism", "hurt", "nuke", "gloomy", "facebook",
+		"geeksforgeeks", "twitter"}
+
+	for _, word := range word_present {
+		db.AddStringToBloomFilter(bf_key, word)
+	}
+
+	for _, word := range word_present {
+		found, _ := db.IsStringPossiblyInBloomFilter(bf_key, word)
+		if !found {
+			t.Fatalf("%s je trebao da bude nadjen, a nije", word)
+		} else {
+			t.Log(word, " nadjen")
+		}
+	}
+
+	for _, word := range word_absent {
+		found, _ := db.IsStringPossiblyInBloomFilter(bf_key, word)
+		if !found {
+			t.Log(word, " nije nadjen")
+		} else {
+			t.Log(word, " je false-positive")
+		}
+	}
+
+	Cleanup()
+
+}
