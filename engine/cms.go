7bf8e8296032922b119fc0f9b12e100e5ca80266
CodeX-cmd <kuvizic.marko@gmail.com>
2023-02-05 14:23:42 +0100

    [Add] Metode za rukovanje cms-om direktno iz engin-a

--- engine/cms.go
+++ engine/cms.go
@@ -1,21 +1,31 @@
 package engine
-//Datoteka uvozi metode za rukovanje Count-Min-Sketch-om u engine
+//Datoteka implementira metode za rukovanje cms-om putem kljuceva iz baze
 import "github.com/darkokos/NAiSP_Projekat/CMS"
 
-func InitCMS(precision float64, certainty float64) *CMS.CMS{
-	cms := &CMS.CMS{}
-	cms.Init(precision, certainty)
-	return cms
+//Dodavanje CMS-a u bazu
+func (engine *DB) PutCMS(key string, cms CMS.CMS) bool{
+	key = "cms." + key
+	return engine.Put(key, cms.Serialize())
+} 
+//Citanje cms-a iz baze
+func  (engine *DB) GetCMS(cmsKey string) *CMS.CMS{
+	return CMS.Deserialize(engine.Get("cms." + cmsKey))
 }
-func AddToCMS(cms *CMS.CMS, key []byte){
+//Dodavanje u cms koji je u bazi, putem kljuca
+func  (engine *DB) AddToCMS(cmsKey string, key []byte) int{
+	cms := engine.GetCMS(cmsKey)
+	if cms == nil{
+		return -1
+	}
 	cms.Add(key)
+	engine.PutCMS(cmsKey, *cms)
+	return 0
 }
-func ReadFromCMS(cms *CMS.CMS, key []byte) uint{
-	return cms.Read(key)
-}
-func SerializeCMS(cms *CMS.CMS) []byte{
-	return cms.Serialize()
-}
-func DeserializeCMS(b []byte) CMS.CMS{
-	return CMS.Deserialize(b)
+//Citanje iz cms-a koji je u bazi, putem kljuca, vraca se statusna promenljiva tipa int i promenljiva tipa uint koja je povratna vrednost cms-a
+func (engine *DB) ReadFromCMS(cmsKey string, key []byte) (int, uint){
+	cms := engine.GetCMS(cmsKey)
+	if cms == nil{
+		return -1, 0
+	}
+	return 0, cms.Read(key)
 }
